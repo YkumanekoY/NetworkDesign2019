befe10ac6a58be4acd76c4b3cfb3c0c58c1b2d45
Yua Akimoto <34079435+YkumanekoY@users.noreply.github.com>
2019-06-28 17:44:14 +0900

    Update app.js

--- network_design/js/app.js
+++ network_design/js/app.js
@@ -6,90 +6,29 @@ var lon = 0;
 
 //天気情報---------------------------------------------------------------------------
 
-$(document).ready(
-    $.when(function() {
-        'use strict'
+$(document).ready(function() {
+    'use strict'
 
-        const APIKEY = "f9afb324cd9adca6010dcb01b05fe097";
+    const APIKEY = "f9afb324cd9adca6010dcb01b05fe097";
 
-        //今日のデータを取り出す
-        const date = new Date();
-        const nowHour = date.getHours();
+    //今日のデータを取り出す
+    const date = new Date();
+    const nowHour = date.getHours();
 
-        //現在位置の取得ができるかどうか
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(success, error);
+    //現在位置の取得ができるかどうか
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
 
-            // 現在位置の取得に成功した場合
-            function success(position) {
-                const positionData = position.coords;
+        // 現在位置の取得に成功した場合
+        function success(position) {
+            const positionData = position.coords;
 
-                //最初の位置の緯度経度取得
-                const lat0 = positionData.latitude;
-                const lon0 = positionData.longitude;
+            //最初の位置の緯度経度取得
+            const lat0 = positionData.latitude;
+            const lon0 = positionData.longitude;
 
-                //緯度経度の表示
-                $('.location').text('現在の位置（' + Math.floor(lat0 * 100) / 100 + ',' + Math.floor(lon0 * 100) / 100 + ')');
-
-                //高度限界まで風船を飛ばすぜ　altは高度
-                var speed = 4.0;
-                latArray.push(lat0);
-                lonArray.push(lon0);
-                lat = lat0;
-                lon = lon0;
-                for (let alt = 2.0; alt < 11000; alt += speed * 10) {
-                    var v2 = new Array();
-                    $.when(
-                        $.ajax({
-                            type: 'GET',
-                            url: "https://api.openweathermap.org/data/2.5/weather",
-                            dataType: "jsonp",
-                            data: "lat=" + lat + "&lon=" + lon + "&appid=" + APIKEY,
-                            //天気データ呼び出し成功時の挙動
-                            success: function(data) {
-                                //console.log(count + " 緯度: " + lat + ", 経度:" + lon);
-                                //console.log("風向き：" + data.wind.deg + " 風速：" + data.wind.speed);
-                                v2 = vincenty(lat, lon, data.wind.deg, data.wind.speed);
-                                lat = v2[0];
-                                lon = v2[1];
-                            },
-                            error: function() {
-                                return;
-                            }
-                        })
-                    ).done(function() {
-                        latArray.push(lat);
-                        lonArray.push(lon);
-                        i = lonArray.length - 1;
-                        console(i);
-                        var addText = latArray[i] + ", " + lonArray[i] + '\n';
-                        // テキストボックスのデータを取得します
-                        var getData = String($("#gps").val());
-                        // 取得データと追記文言をくっつけて出力します
-                        $("#gps").val(getData + addText);
-                    });
-                }
-            }
-
-            //現在位置の取得に失敗した場合
-            function error(error) {
-                alert("位置情報が取得できなかったため、東京の天気を表示します");
-                $('.location').text('東京');
-
-                TokyoWeather();
-
-            }
-            //現在位置がそもそも取得できない場合
-        } else {
-            alert("位置情報が取得できなかったため、東京の天気を表示します");
-            $('.location').text('東京');
-
-            TokyoWeather();
-        }
-
-        //東京の天気
-        function TokyoWeather() {
-
+            //緯度経度の表示
+            $('.location').text('現在の位置（' + Math.floor(lat0 * 100) / 100 + ',' + Math.floor(lon0 * 100) / 100 + ')');
             //現在の天気データ呼び出し
             $.ajax({
                 url: "https://api.openweathermap.org/data/2.5/weather",
@@ -118,24 +57,53 @@ $(document).ready(
             });
         }
 
-        function sleep(waitMsec) {
-            var startMsec = new Date();
+        //現在位置の取得に失敗した場合
+        function error(error) {
+            alert("位置情報が取得できなかったため、東京の天気を表示します");
+            $('.location').text('東京');
 
-            // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
-            while (new Date() - startMsec < waitMsec);
+            TokyoWeather();
+
         }
-    }).done(function() {
-            console.log(latArray.length);
-            console.log(lonArray.length);
-            console.log(lonArray);
-            for (var i = 0; i < latArray.length && i < lonArray.length; i++) {
-                $("#gps").val(latArray[i] + ", " + lonArray[i] + '\n');
-                console(i);
+        //現在位置がそもそも取得できない場合
+    } else {
+        alert("位置情報が取得できなかったため、東京の天気を表示します");
+        $('.location').text('東京');
+
+        TokyoWeather();
+    }
+
+    //東京の天気
+    function TokyoWeather() {
+
+        //現在の天気データ呼び出し
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather",
+            dataType: "jsonp",
+            data: "q=Tokyo,jp&appid=" + APIKEY,
+            //天気データ呼び出し成功時の挙動
+            success: function(data) {
+                if (data.weather[0].main === "Sunny" || data.weather[0].main === "Clear") {
+                    $('body').css('background-image', 'url(Sunny.jpg)');
+                    $('.dayWeather').text("晴れ");
+                } else if (data.weather[0].main === "Rain") {
+                    $('body').css('background-image', 'url(Rain.jpg)');
+                    $('.dayWeather').text("雨");
+                } else if (data.weather[0].main === "Clouds") {
+                    $('body').css('background-image', 'url(Cloudy.jpg)');
+                    $('.dayWeather').text("くもり");
+                } else if (data.weather[0].main === "Snow") {
+                    $('body').css('background-image', 'url(Snowy.jpg)');
+                    $('.dayWeather').text("雪");
+                }
+
+                //各データの表示
+                $('.nowTemp').text(Math.floor((data.main.temp - 273.15) * 10) / 10);
+                $('.dayWeatherIcon').attr('src', 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png ');
             }
-        }
-
-    )
-);
+        });
+    }
+});
 
 //---------------------------------------------------------------------------
 
@@ -156,57 +124,4 @@ function getAzimuth(degree) {
         return dname[count];
     }
     return dname[count];
-}
-
-//5秒ごとに位置情報を更新
-//setTimeout("targetPos(lat, lon, distance, degree)", 3000);
-
-
-//---ここから風船の目的地計算用--------------------------
-var check;
-var Radius_long = 6378137.0;
-var Henpei = 1 / 298.257222101;
-var Radius_short = Radius_long * (1 - Henpei); // 6356752.314 
-
-function doRad(a) {
-    return a / 180 * Math.PI;
-}
-
-function radDo(a) {
-    return a * 180 / Math.PI;
-}
-
-function xy(x, y) {
-    return Math.pow(x, y);
-}
-
-function vincenty(lat1, lng1, alpha12, length) {
-    lat1 = doRad(lat1);
-    lng1 = doRad(lng1);
-    alpha12 = doRad(alpha12);
-
-    var U1 = Math.atan((1 - Henpei) * Math.tan(lat1));
-    var sigma1 = Math.atan(Math.tan(U1) / Math.cos(alpha12));
-    var alpha = Math.asin(Math.cos(U1) * Math.sin(alpha12));
-    var u2 = xy(Math.cos(alpha), 2) * (xy(Radius_long, 2) - xy(Radius_short, 2)) / xy(Radius_short, 2);
-    var A = 1 + (u2 / 16384) * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)));
-    var B = (u2 / 1024) * (256 + u2 * (-128 + u2 * (74 - 47 * u2)));
-    var sigma = length / Radius_short / A;
-    do {
-        var sigma0 = sigma;
-        var dm2 = 2 * sigma1 + sigma;
-        var x = Math.cos(sigma) * (-1 + 2 * xy(Math.cos(dm2), 2)) - B / 6 * Math.cos(dm2) * (-3 + 4 * xy(Math.sin(dm2), 2)) * (-3 + 4 * xy(Math.cos(dm2), 2));
-        var dSigma = B * Math.sin(sigma) * (Math.cos(dm2) + B / 4 * x);
-        sigma = length / Radius_short / A + dSigma;
-    } while (Math.abs(sigma0 - sigma) > 1e-9);
-
-    var x = Math.sin(U1) * Math.cos(sigma) + Math.cos(U1) * Math.sin(sigma) * Math.cos(alpha12)
-    var y = (1 - Henpei) * xy(xy(Math.sin(alpha), 2) + xy(Math.sin(U1) * Math.sin(sigma) - Math.cos(U1) * Math.cos(sigma) * Math.cos(alpha12), 2), 1 / 2);
-    var lamda = Math.sin(sigma) * Math.sin(alpha12) / (Math.cos(U1) * Math.cos(sigma) - Math.sin(U1) * Math.sin(sigma) * Math.cos(alpha12));
-    lamda = Math.atan(lamda);
-    var C = (Henpei / 16) * xy(Math.cos(alpha), 2) * (4 + Henpei * (4 - 3 * xy(Math.cos(alpha), 2)));
-    var z = Math.cos(dm2) + C * Math.cos(sigma) * (-1 + 2 * xy(Math.cos(dm2), 2));
-    var omega = lamda - (1 - C) * Henpei * Math.sin(alpha) * (sigma + C * Math.sin(sigma) * z);
-    //console.log(radDo(Math.atan(x / y)) + ", " + radDo(lng1 + omega))
-    return [radDo(Math.atan(x / y)), radDo(lng1 + omega)];
 }

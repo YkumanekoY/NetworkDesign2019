14b295d53579ad28399d22d739af4805e9ab0e2e
Yua Akimoto <kumanekoy@gmail.com>
2019-07-12 18:47:16 +0900

    Update app.js

--- network_design/js/app.js
+++ network_design/js/app.js
@@ -7,97 +7,91 @@ var lon = 0;
 
 //天気情報---------------------------------------------------------------------------
 
-$(document).ready(
-    $.when(function() {
-        // 処理前に Loading 画像を表示
-        dispLoading("処理中...");
+$(document).ready(function() {
+    dispLoading("処理中...")
 
-        // 非同期処理
-        'use strict'
-        const APIKEY = "f9afb324cd9adca6010dcb01b05fe097";
+    setWeather();
+});
 
-        //今日のデータを取り出す
-        const date = new Date();
-        const nowHour = date.getHours();
+function setWeather() {
+    // // 処理前に Loading 画像を表示
+    // dispLoading("処理中...");
 
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(success, error);
+    // 非同期処理
+    'use strict'
+    const APIKEY = "f9afb324cd9adca6010dcb01b05fe097";
 
-            // 現在位置の取得に成功した場合
-            function success(position) {
-                const positionData = position.coords;
+    //今日のデータを取り出す
+    const date = new Date();
+    const nowHour = date.getHours();
 
-                //最初の位置の緯度経度取得
-                const lat0 = positionData.latitude;
-                const lon0 = positionData.longitude;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
 
-                //緯度経度の表示
-                $('.location').text('現在の位置（' + Math.floor(lat0 * 100) / 100 + ',' + Math.floor(lon0 * 100) / 100 + ')');
+        // 現在位置の取得に成功した場合
+        function success(position) {
+            const positionData = position.coords;
 
-                //高度限界まで風船を飛ばすぜ　altは高度
-                var speed = 4.0;
-                latArray.push(lat0);
-                lonArray.push(lon0);
-                lat = lat0;
-                lon = lon0;
-                for (let alt = 2.0; alt < 11000; alt += speed * 10) {
-                    var v2 = new Array();
-                    $.ajax({
-                        type: 'GET',
-                        url: "https://api.openweathermap.org/data/2.5/weather",
-                        dataType: "jsonp",
-                        data: "lat=" + lat + "&lon=" + lon + "&appid=" + APIKEY,
-                        //天気データ呼び出し成功時の挙動
-                        // success: function(data) {
-                        //     //console.log(count + " 緯度: " + lat + ", 経度:" + lon);
-                        //     //console.log("風向き：" + data.wind.deg + " 風速：" + data.wind.speed);
-                        //     v2 = vincenty(lat, lon, data.wind.deg, data.wind.speed);
-                        //     lat = v2[0];
-                        //     lon = v2[1];
+            //最初の位置の緯度経度取得
+            const lat0 = positionData.latitude;
+            const lon0 = positionData.longitude;
 
-                        //     console.log(i);
-                        // },
-                        // error: function() {
-                        //     return;
-                        // }
-                    }).done(function(data) {
-                        v2 = vincenty(lat, lon, data.wind.deg, data.wind.speed);
+            //緯度経度の表示
+            $('.location').text('現在の位置（' + Math.floor(lat0 * 100) / 100 + ',' + Math.floor(lon0 * 100) / 100 + ')');
+
+            //高度限界まで風船を飛ばすぜ　altは高度
+            var speed = 4.0;
+            latArray.push(lat0);
+            lonArray.push(lon0);
+            lat = lat0;
+            lon = lon0;
+            for (let alt = 2.0; alt < 11000; alt += speed * 10) {
+                var v2 = new Array();
+                $.ajax({
+                    type: 'GET',
+                    url: "https://api.openweathermap.org/data/2.5/weather",
+                    dataType: "jsonp",
+                    data: "lat=" + lat + "&lon=" + lon + "&appid=" + APIKEY,
+                    //天気データ呼び出し成功時の挙動
+                    success: function(data) {
+                        //console.log(count + " 緯度: " + lat + ", 経度:" + lon);
+                        //console.log("風向き：" + data.wind.deg + " 風速：" + data.wind.speed);
+                        v2 = vincenty(lat, lon, data.wind.deg, data.wind.speed * 30);
                         lat = v2[0];
                         lon = v2[1];
-                        console.log(i);
-                    }).error(function() {
-                        alert("取得できませんでした");
-                        return lat = 0, lon = 0;
-                    }).always(function() {
-                        latArray.push(lat);
-                        lonArray.push(lon);
-                        i = lonArray.length - 1;
-                        console.log(i);
-                    });
-                }
+                        console.log(alt + "city: " + data.name);
+                    },
+                    error: function() {
+                        return;
+                    }
+                }).done(function() {
+                    latArray.push(lat);
+                    lonArray.push(lon);
+                    //console.log(latArray);
+                    if (alt + speed * 10 > 11000) {
+                        removeLoading();
+                        initMap();
+                    }
+                });
             }
+        }
 
-            //現在位置の取得に失敗した場合
-            function error(error) {
-                alert("位置情報が取得できなかったため、東京の天気を表示します");
-                $('.location').text('東京');
-
-                TokyoWeather();
-
-            }
-            //現在位置がそもそも取得できない場合
-        } else {
+        //現在位置の取得に失敗した場合
+        function error(error) {
             alert("位置情報が取得できなかったため、東京の天気を表示します");
             $('.location').text('東京');
 
             TokyoWeather();
+
         }
-    }).always(function() {
-        console.log(lonArray);
-        removeLoading();
-        initMap();
-    })
-);
+        //現在位置がそもそも取得できない場合
+    } else {
+        alert("位置情報が取得できなかったため、東京の天気を表示します");
+        $('.location').text('東京');
+
+        TokyoWeather();
+    }
+}
 
 /* ------------------------------
  Loading イメージ表示関数
@@ -201,164 +195,178 @@ function vincenty(lat1, lng1, alpha12, length) {
 function initMap() {
     if (navigator.geolocation) {
         // 現在地を取得
-        navigator.geolocation.getCurrentPosition(
-            // 取得成功した場合
-            function(position) {
-                // 緯度・経度を変数に格納
-                var mapLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                var mapArea = document.getElementById('sample');
-                var mapOptions = {
-                    disableDefaultUI: true,
-                    center: mapLatLng,
-                    zoom: 15,
-                    styles: [ /*マップの見た目*/ {
-                        "featureType": "all",
-                        "elementType": "labels.text.fill",
-                        "stylers": [{
-                            "color": "#ffffff"
-                        }]
-                    }, {
-                        "featureType": "all",
-                        "elementType": "labels.text.stroke",
-                        "stylers": [{
-                            "color": "#A28E8B"
-                        }]
-                    }, {
-                        "featureType": "all",
-                        "elementType": "labels.icon",
-                        "stylers": [{
-                            "visibility": "off"
-                        }]
-                    }, {
-                        "featureType": "administrative",
-                        "elementType": "geometry.fill",
-                        "stylers": [{
-                            "color": "#F7EFED"
-                        }]
-                    }, {
-                        "featureType": "administrative",
-                        "elementType": "geometry.stroke",
-                        "stylers": [{
-                            "color": "#F7EFED"
-                        }, {
-                            "weight": 1.2
-                        }]
-                    }, {
-                        "featureType": "administrative.locality",
-                        "elementType": "geometry.fill",
-                        "stylers": [{
-                            "lightness": "-1"
-                        }]
-                    }, {
-                        "featureType": "administrative.neighborhood",
-                        "elementType": "labels.text.fill",
-                        "stylers": [{
-                            "lightness": "0"
-                        }, {
-                            "saturation": "0"
-                        }]
-                    }, {
-                        "featureType": "administrative.neighborhood",
-                        "elementType": "labels.text.stroke",
-                        "stylers": [{
-                            "weight": "0.01"
-                        }]
-                    }, {
-                        "featureType": "administrative.land_parcel",
-                        "elementType": "labels.text.stroke",
-                        "stylers": [{
-                            "weight": "0.01"
-                        }]
-                    }, {
-                        "featureType": "landscape",
-                        "elementType": "geometry",
-                        "stylers": [{
-                            "color": "#F7EFED"
-                        }]
-                    }, {
-                        "featureType": "poi",
-                        "elementType": "geometry",
-                        "stylers": [{
-                            "color": "#C4B4B3"
-                        }]
-                    }, {
-                        "featureType": "poi.park",
-                        "elementType": "labels.icon",
-                        "stylers": [{
-                            visibility: "simplified"
-                        }, ],
-                    }, {
-                        "featureType": "poi.park",
-                        "elementType": "labels.icon",
-                        "stylers": [{
-                            "color": "#C4B4B3"
-                        }]
-                    }, {
-                        "featureType": "road",
-                        "elementType": "geometry.stroke",
-                        "stylers": [{
-                            "visibility": "off"
-                        }]
-                    }, {
-                        "featureType": "road.highway",
-                        "elementType": "geometry.fill",
-                        "stylers": [{
-                            "color": "#C4B4B3"
-                        }]
-                    }, {
-                        "featureType": "road.highway.controlled_access",
-                        "elementType": "geometry.stroke",
-                        "stylers": [{
-                            "color": "#C4B4B3"
-                        }]
-                    }, {
-                        "featureType": "road.arterial",
-                        "elementType": "geometry",
-                        "stylers": [{
-                            "color": "#C4B4B3"
-                        }]
-                    }, {
-                        "featureType": "road.local",
-                        "elementType": "geometry",
-                        "stylers": [{
-                            "color": "#C4B4B3"
-                        }]
-                    }, {
-                        "featureType": "transit",
-                        "elementType": "geometry",
-                        "stylers": [{
-                            "color": "#A28E8B"
-                        }]
-                    }, {
-                        "featureType": "water",
-                        "elementType": "geometry",
-                        "stylers": [{
-                            "color": "#61D4E2"
-                        }]
-                    }]
-                };
+        var mapLatLng = new google.maps.LatLng(latArray[0], lonArray[0]);
+        var mapArea = document.getElementById('sample');
+        var mapOptions = {
+            disableDefaultUI: true,
+            center: mapLatLng,
+            zoom: 15,
+            styles: [ /*マップの見た目*/ {
+                "featureType": "all",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#ffffff"
+                }]
+            }, {
+                "featureType": "all",
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "color": "#A28E8B"
+                }]
+            }, {
+                "featureType": "all",
+                "elementType": "labels.icon",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            }, {
+                "featureType": "administrative",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#F7EFED"
+                }]
+            }, {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "color": "#F7EFED"
+                }, {
+                    "weight": 1.2
+                }]
+            }, {
+                "featureType": "administrative.locality",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "lightness": "-1"
+                }]
+            }, {
+                "featureType": "administrative.neighborhood",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "lightness": "0"
+                }, {
+                    "saturation": "0"
+                }]
+            }, {
+                "featureType": "administrative.neighborhood",
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "weight": "0.01"
+                }]
+            }, {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "weight": "0.01"
+                }]
+            }, {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#F7EFED"
+                }]
+            }, {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#C4B4B3"
+                }]
+            }, {
+                "featureType": "poi.park",
+                "elementType": "labels.icon",
+                "stylers": [{
+                    visibility: "simplified"
+                }, ],
+            }, {
+                "featureType": "poi.park",
+                "elementType": "labels.icon",
+                "stylers": [{
+                    "color": "#C4B4B3"
+                }]
+            }, {
+                "featureType": "road",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            }, {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#C4B4B3"
+                }]
+            }, {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "color": "#C4B4B3"
+                }]
+            }, {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#C4B4B3"
+                }]
+            }, {
+                "featureType": "road.local",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#C4B4B3"
+                }]
+            }, {
+                "featureType": "transit",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#A28E8B"
+                }]
+            }, {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#61D4E2"
+                }]
+            }]
+        };
 
-                var map = new google.maps.Map(mapArea, mapOptions);
-                var input = document.getElementById('pac-input');
-                var searchBox = new google.maps.places.SearchBox(input);
-                map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+        var map = new google.maps.Map(mapArea, mapOptions);
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
 
-                map.addListener('bounds_changed', function() {
-                    searchBox.setBounds(map.getBounds());
-                });
+        map.addListener('bounds_changed', function() {
+            searchBox.setBounds(map.getBounds());
+        });
 
-                var markerOptions = {
-                    map: map,
-                    position: mapLatLng,
-                    icon: new google.maps.MarkerImage(
-                        'assets/images/balloon.png',
-                    ),
-                };
+        var markerOptions = {
+            map: map,
+            position: mapLatLng,
+            icon: new google.maps.MarkerImage(
+                'assets/images/balloon.png',
+            ),
+        };
 
-                var marker = new google.maps.Marker(markerOptions);
-            }
-        );
-    } else {
-        alert("この端末では位置情報が取得できません");
+        var marker = new google.maps.Marker(markerOptions);
+
+        //線の描画
+        var flightPlanCoordinates = [
+            // { lat: 37.772, lng: -122.214 },
+            // {lat: 21.291, lng: -157.821},
+            // {lat: -18.142, lng: 178.431},n.toFixed(1)
+            // {lat: -27.467, lng: 153.027}
+        ];
+        for (let i = 0; i < latArray.length && i < lonArray.length; i++) {
+            var latlon = { lat: latArray[i], lng: lonArray[i] };
+            flightPlanCoordinates.push(latlon)
+        }
+
+        var flightPath = new google.maps.Polyline({
+            path: flightPlanCoordinates,
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+
+        flightPath.setMap(map);
     }
 }

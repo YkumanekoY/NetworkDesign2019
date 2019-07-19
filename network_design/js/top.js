57be6ce64d0350cffa091cfa786989f8d9432cef
津久井千佳 <chika@tsukuichikanoMacBook-Pro.local>
2019-07-19 14:49:36 +0900

    フェードインとフェードアウトが出来るようになった

--- network_design/js/top.js
+++ network_design/js/top.js
@@ -36,10 +36,54 @@ $(function() {
   });
 });*/
 
+/*var pics_src = new Array("../images/balloon4.png");
+            var num = 0;
+
+            function slideshow(){
+                if (num == 1) {
+                    num = 0;
+                    console.log("reset");
+                }
+                else {
+                    num ++;
+                    console.log("plus");
+                }
+                document.getElementById("btn1").src=pics_src[num];
+            }*/
+
+$(window).on('load', function(){
+              $('body').removeClass('fadeout');
+});
+
 $('#btn1').click(function(){
   console.log("fo");
   $('#bodyfadeout').fadeOut("slow");
+  setTimeout(function(){
+    window.location = "./html/map.html";  // 0.8秒後に取得したURLに遷移
+  }, 800);
 });
+
+/*$(function() {
+  // ハッシュリンク(#)と別ウィンドウでページを開く場合はスルー
+  $('a:not([href^="#"]):not([target])').on('click', function(e){
+    e.preventDefault(); // ナビゲートをキャンセル
+    url = $(this).attr('href'); // 遷移先のURLを取得
+    if (url !== './html/map.html') {
+      $('body').addClass('fadeout');  // bodyに class="fadeout"を挿入
+      setTimeout(function(){
+        window.location = url;  // 0.8秒後に取得したURLに遷移
+      }, 800);
+    }
+    return false;
+  });
+  console.log("fo");
+});*/
+
+/*$('#btn1').click(function(){
+  console.log("fo");
+  $('#bodyfadeout').fadeOut("slow");
+});*/
+
 
 /*$('#btn1').click(function(){
 $('#bodyfadein').hide().fadeIn("slow");
